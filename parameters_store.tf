c639e6fbeffb02f3c56f95fe79f05db5e5f3bba4
waynerferreira <waynerferreira@hotmail.com>
2025-04-08 22:54:21 -0300

    fix: path parameters

--- parameters_store.tf
+++ parameters_store.tf
@@ -1,24 +1,24 @@
 resource "aws_ssm_parameter" "vpc" {
-    name = format("%s/vpc/vpc_id", var.project_name)
+    name = format("/%s/vpc/vpc_id", var.project_name)
     type = "String"
     value = aws_vpc.main.id
 }
 
 ################ subnet public ####################
 resource "aws_ssm_parameter" "public_1a" {
-  name = format("%s-/vpc/subnet_public_1a", var.project_name)
+  name = format("/%s-/vpc/subnet_public_1a", var.project_name)
   type = "String"
   value = aws_subnet.public_subnet_1a.id
 }
 
 resource "aws_ssm_parameter" "public_1b" {
-  name = format("%s-/vpc/subnet_public_1b", var.project_name)
+  name = format("/%s-/vpc/subnet_public_1b", var.project_name)
   type = "String"
   value = aws_subnet.public_subnet_1b.id
 }
 
 resource "aws_ssm_parameter" "public_1c" {
-  name = format("%s-/vpc/subnet_public_1c", var.project_name)
+  name = format("/%s-/vpc/subnet_public_1c", var.project_name)
   type = "String"
   value = aws_subnet.public_subnet_1c.id
 }
@@ -27,19 +27,19 @@ resource "aws_ssm_parameter" "public_1c" {
 
 
 resource "aws_ssm_parameter" "private_1a" {
-  name = format("%s-/vpc/subnet_private_1a", var.project_name)
+  name = format("/%s-/vpc/subnet_private_1a", var.project_name)
   type = "String"
   value = aws_subnet.private_subnet_1a.id
 }
 
 resource "aws_ssm_parameter" "private_1b" {
-  name = format("%s-/vpc/subnet_private_1b", var.project_name)
+  name = format("/%s-/vpc/subnet_private_1b", var.project_name)
   type = "String"
   value = aws_subnet.private_subnet_1b.id
 }
 
 resource "aws_ssm_parameter" "private_1c" {
-  name = format("%s-/vpc/subnet_private_1c", var.project_name)
+  name = format("/%s-/vpc/subnet_private_1c", var.project_name)
   type = "String"
   value = aws_subnet.private_subnet_1c.id
 }
@@ -47,19 +47,19 @@ resource "aws_ssm_parameter" "private_1c" {
 ############ subnet database #####################
 
 resource "aws_ssm_parameter" "databases_1a" {
-  name = format("%s-/vpc/subnet_databases_1a", var.project_name)
+  name = format("/%s-/vpc/subnet_databases_1a", var.project_name)
   type = "String"
   value = aws_subnet.databases_subnet_1a.id
 }
 
 resource "aws_ssm_parameter" "databases_1b" {
-  name = format("%s-/vpc/subnet_databases_1b", var.project_name)
+  name = format("/%s-/vpc/subnet_databases_1b", var.project_name)
   type = "String"
   value = aws_subnet.databases_subnet_1b.id
 }
 
 resource "aws_ssm_parameter" "databases_1c" {
-  name = format("%s-/vpc/subnet_databases_1c", var.project_name)
+  name = format("/%s-/vpc/subnet_databases_1c", var.project_name)
   type = "String"
   value = aws_subnet.databases_subnet_1c.id
 }
